523c77aa461409f3bfc2a30479764d22f78a815e
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-08 00:18:02 +0100

    Day2 project done

---
+++ Day2/project/tipcalc.py
@@ -0,0 +1,12 @@
+#bill + tax calculator
+print("Welcome to the tip calculator")
+total_bill = input("What is the total of the bill?")
+percentage = input("WHat would tip would you like to give? 10, 12 or 15?")
+split_people = input("How many people to split the bill?")
+percentage_to_float= float(percentage)
+total_bill_toFloat =float(total_bill) 
+
+final_pay = total_bill_toFloat + ((percentage_to_float /100)* total_bill_toFloat)
+perPerson = final_pay / int(split_people)
+result = round(perPerson,2)
+print(f"Each person should pay: {result}")
