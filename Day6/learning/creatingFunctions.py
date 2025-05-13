f53ebd6beb9fef883e0cd3879a499285ced8fef1
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-13 18:54:36 +0100

    Most of Day6 work was done on a website(just indentation, while loops and problem solving

---
+++ Day6/learning/creatingFunctions.py
@@ -0,0 +1,9 @@
+#learning to create user defined functions 
+
+def my_function():
+    print("Happy Birthday to you")
+
+my_function()
+#also learnt about indentations and all
+
+#range(start, stop , step) //sequence does not include the stop niumber
