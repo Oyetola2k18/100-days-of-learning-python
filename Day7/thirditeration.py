0dac3c386b6026eed93bcc3f3105a182254d11ac
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-14 21:57:36 +0100

    Still not done with day7 but we making progress (learning problem solving)

--- Day7/thirditeration.py
+++ Day7/thirditeration.py
@@ -1,26 +1,63 @@
+#Hangman game , second phase generating a
+#Angela way
+# import random
+# word_list = ["ardvark", "baboon", "camel"]
+
+# chosen_word = word_list[random.randint(0,len(word_list)-1)]
+
+# #testing code
+# print(f"THe chosen word is {chosen_word}")
+
+
+# display = []
+# for letter in chosen_word:
+#     display+="_"
+
+# end_of_game = False
+# while not end_of_game:
+
+#     user_choice = input("Guess a letter:").lower()
+
+#     for x in range(0,len(chosen_word)):
+#         if user_choice == chosen_word[x]:
+#             display[x]= user_choice
+
+#     print(display)
+
+#     if "_" not in display:
+#         end_of_game = True
+#         print("You win")
+
+
+
+
+
 #the third iteration of the project
 #Hangman game , third phase
+#my way
 import random
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = word_list[random.randint(0,len(word_list)-1)]
-
-#testing code
 print(f"THe chosen word is {chosen_word}")
 
-display = []
-
-while display.count("_") <=0:
+user_choice = input("Guess a letter:").lower()
+display=[]
+for letter in chosen_word:
+    display+="_"
+    
+#this is the loop to assign the users choice to the display if it exists
+for x in range(0,len(chosen_word)):
+    if user_choice == chosen_word[x]:
+        display[x]= user_choice
+print(display)
+while display.count("_") > 0:
     user_choice = input("Guess a letter:").lower()
-    #this holds the list  of _
-    #this loop is to add as much "_" in the letters of the word
-    for letter in chosen_word:
-        display+="_"
-
-    #this is the loop to assign the users choice to the display if it exists
     for x in range(0,len(chosen_word)):
         if user_choice == chosen_word[x]:
             display[x]= user_choice
+    print(display)
 
-
-print(display)
+    if not "_" in display:
+        print("YOu win")
+        exit()
