29d552a5f41b32b50d768ee3cfccf2e15d0118be
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-17 16:56:01 +0100

    Caeser's cipher project done

--- Day8/CaserCipher/CaserCipherFinal.py
+++ Day8/CaserCipher/CaserCipherFinal.py
@@ -1,4 +1,5 @@
 
+#one thing about the modulus function, if your number you are divviding by is less than the denominator, it will return the numerator
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -6,6 +7,9 @@ def caeser(Words,shifts,direction):
     Final_Message = ""
     if direction == "encode":
         for x in range(0,len(Words)):
+            if(Words[x] not in alphabet):
+                Final_Message+=Words[x]
+                continue
             position = int(alphabet.index(Words[x]))
             new_position = position + shifts
             if new_position >= alphabet.index('z'):
@@ -15,27 +19,26 @@ def caeser(Words,shifts,direction):
         print(f"The encrypted message is: {Final_Message}")
     elif direction == "decode":
         for x in range(0,len(Words)):
+            if(Words[x] not in alphabet):
+                Final_Message+=Words[x]
+                continue
             position = int(alphabet.index(Words[x]))
             new_position = position - shifts
 
-            Final_Message+=alphabet[new_position]
+            Final_Message+=alphabet[new_position%26]
         print(f"The decrypted message is: {Final_Message}")
     
 import art
 print(art.logo)
 
-end_of_game  = False
-while end_of_game == False:
+should_continue = True
+while should_continue == True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     caeser(direction=direction,Words=text,shifts=shift)
-    run = input("Type 'yes'if you wanna go again , otherwise type 'NO' ")).lower()
-    if run == "yes":
-        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        caeser(direction=direction,Words=text,shifts=shift)
-    elif run == "no":
-        print("Alright bye")
-        end_of_game = True
+    
+    run = input("Type 'yes'if you wanna go again , otherwise type 'NO' ").lower()
+    if run == "no":
+        print("Aiit bye")
+        should_continue=False
