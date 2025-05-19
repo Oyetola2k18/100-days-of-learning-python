759c96290b3e2cc9bb68ef39fc8a8af98ac21b31
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-20 00:42:42 +0100

    Day 10 completed

---
+++ Day10/learning/diffPrintReturn.py
@@ -0,0 +1,4 @@
+#differentiating between the use of print and the use of rreturn 
+#The print function means we want to print something to the console
+#the return keyword is just to give an output that can later be assigned to a variable
+
--- Day10/project/Calculator.py
+++ Day10/project/Calculator.py
@@ -2,6 +2,7 @@
 
 #functions the calculator performs
 #add
+from art import logo
 def add(n1,n2):
     return n1 + n2
 
@@ -26,13 +27,42 @@ operations={
     "/":divide,
 }
 
-num1=int(input("Input the first number:"))
-num2=int(input("Input the Second number:"))
+# answer = operations[operation_symbol](num1,num2)
+# or 
+# operation_function = operations[operation_symbol]
+# then we can use the operation function as a function
+# operation_function(num1,num2)
+# print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-for opp in operations:
-    print(opp)
-operation_symbol=input("Pick an operation from the line above: ")
+# operation_symbol = input("Pick an operation from the line above: ")
+# operation_function2 =operations[operation_symbol]
 
-answer = operations[operation_symbol](num1,num2)
+# num3 = int(input("input the new number"))
+# answer2 = operation_function2(answer,num3)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+# print(f"{answer} {operation_symbol} {num3} = {answer2}")
+
+#making use of recursion, this is when a function calls itself in itself lollll
+def calculator():
+    print(logo)
+    end_calc = False
+    num1=float(input("Input the first number:"))
+
+    while end_calc==False:
+        for opp in operations:
+            print(opp)
+        operation_symbol=input("Pick an operation from the line above: ")
+        num2=float(input("Input the next number:"))
+
+        operation_function = operations[operation_symbol]
+        answer = operation_function(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice =input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start the calculator again:")
+        if choice == 'n':
+            end_calc = True
+            calculator()#if user says no, it exits the while loop and starts the function again
+        if choice == 'y':
+            num1 = answer
+#where the program actually starts from
+calculator()
