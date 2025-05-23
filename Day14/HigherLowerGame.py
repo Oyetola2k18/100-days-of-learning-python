ba3ec0559fc7974d737c7ccf938b66b9055f924e
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-23 20:47:14 +0100

    Day 14's project in progress

--- Day14/HigherLowerGame.py
+++ Day14/HigherLowerGame.py
@@ -1,1 +1,18 @@
 #Higher lower game
+from data import data
+import random
+def generate_people():
+    """Returns Name,Follower count snd other info about the Celebrity"""
+    return data[random.randint(0,(len(data)-1))]
+
+def person():
+    celeb = generate_people()
+    name = celeb["name"]
+    follower_count = celeb['follower_count']
+    description=celeb["description"]
+    country=celeb["country"]
+    def inner():
+        return follower_count 
+    return (f"{name},{description},from {country}"),inner
+
+person1 = person()
