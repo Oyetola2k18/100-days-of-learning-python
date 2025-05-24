853a4f92ac553e91df6d05f5717eb91a6c379a1f
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-24 19:39:09 +0100

    Day 14 doneee

--- Day14/HigherLowerGame.py
+++ Day14/HigherLowerGame.py
@@ -1,6 +1,8 @@
 #Higher lower game
 from data import data
 import random
+from replit import clear
+from art import logo,vs
 def generate_people():
     """Returns Name,Follower count snd other info about the Celebrity"""
     return data[random.randint(0,(len(data)-1))]
@@ -8,7 +10,6 @@ def generate_people():
 def compare(count1,count2):
     """Take in two values and returns the highest value"""
     return max(count1,count2)
-
 
 def person():
     celeb = generate_people()
@@ -22,27 +23,46 @@ def person():
     return info,follower_count
 #learnt a new thing about the return keywork that it can return multiple values
 
+print(logo)
+Score = 0
+
 info,count = person()
-info2,count2 = person()
-print(f"Compare A:{info}")
-#testing code
-print(count)
-print(f"Compare A:{info2}")
-#testing code
-print(count2)
-choice = input("Who has more followers? Type 'A' or 'B':").lower()
-highest_value = compare(count,count2)
 
-#testing code 
-print(highest_value)
+end_game = False
+while not end_game:
+    info2,count2 = person()
+    print(f"Compare A:{info}")
+    #testing code
+    # print(count)
+    print(vs)
+    print(f"Compare B:{info2}")
+    #testing code
+    # print(count2)
+    choice = input("Who has more followers? Type 'A' or 'B':").lower()
+    highest_value = compare(count,count2)
 
-if choice == 'a':
-    if count == highest_value:
-        print("YOu are correct")
+    #testing code 
+    # print(highest_value)
+
+    if choice == 'a':
+        if count == highest_value:
+            print("YOu are correct")
+            Score+=1
+            info = info2
+            count = count2
+            clear()
+        else:
+            print(f"You lose, Game Over: YOur total score is {Score}")
+            end_game = True
+    elif choice == 'b':
+        if count2 == highest_value:
+            print("YOu are correct")
+            Score+=1
+            info = info2
+            count = count2
+            clear()
+        else:
+            print(f"You lose, Game Over: YOur total score is {Score}")
+            end_game = True
     else:
-        print("You lose")
-elif choice == 'b':
-    if count2 == highest_value:
-        print("YOu are correct")
-    else:
-        print("You lose")
+        end_game = True
