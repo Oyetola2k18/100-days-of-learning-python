dd115aa6bcc640a4768afa01aea497da7937d267
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-24 18:39:00 +0100

    DAY 14 still in progress , i have been delaying this project too much

--- Day14/HigherLowerGame.py
+++ Day14/HigherLowerGame.py
@@ -5,14 +5,44 @@ def generate_people():
     """Returns Name,Follower count snd other info about the Celebrity"""
     return data[random.randint(0,(len(data)-1))]
 
+def compare(count1,count2):
+    """Take in two values and returns the highest value"""
+    return max(count1,count2)
+
+
 def person():
     celeb = generate_people()
     name = celeb["name"]
     follower_count = celeb['follower_count']
     description=celeb["description"]
     country=celeb["country"]
-    def inner():
-        return follower_count 
-    return (f"{name},{description},from {country}"),inner
+    # def inner():
+    #     return follower_count 
+    info = (f"{name}, {description},from {country}")
+    return info,follower_count
+#learnt a new thing about the return keywork that it can return multiple values
 
-person1 = person()
+info,count = person()
+info2,count2 = person()
+print(f"Compare A:{info}")
+#testing code
+print(count)
+print(f"Compare A:{info2}")
+#testing code
+print(count2)
+choice = input("Who has more followers? Type 'A' or 'B':").lower()
+highest_value = compare(count,count2)
+
+#testing code 
+print(highest_value)
+
+if choice == 'a':
+    if count == highest_value:
+        print("YOu are correct")
+    else:
+        print("You lose")
+elif choice == 'b':
+    if count2 == highest_value:
+        print("YOu are correct")
+    else:
+        print("You lose")
