40153f2810dfd43dcf01ff3efb11d7e8496d8f95
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-25 22:28:04 +0100

    Day 15 starts

---
+++ Day15/learning/pycharmFeatures.py
@@ -0,0 +1,12 @@
+#Pycharm helps with spellchecking
+#Just learning basic things that pycharm helps with
+
+#learnt about refactor then rename
+def add(n1, n2):
+    total = n1 + n2
+    return  total
+
+
+print(add(5, 2))
+
+print("ADD TWO NUMBERS")
---
+++ Day15/project/main.py
@@ -0,0 +1,26 @@
+#Simulating a real life coffee machine
+
+#makes 3 hot flavours
+#Espresso- 50ml Water,18g coffee
+#latte-200ml water,24g coffee, 150ml Milk
+#cappuccino- 250ml water,24g coffee, 100ml water
+
+#Espresso-$1.50
+#latte-$2.50
+#cappuccino- $3.00
+
+#coffee machine's resources: 300ml water, 200ml milk , 100g coffee
+
+#coin operated:
+#list of american coins:
+#penny - 1  ($0.01)
+#nickel - 5 cents ($0.05)
+#dime - 10 cents ($0.10)
+#quarter - 25 cents ($0.25)
+
+#program requirements
+#Print reports(the resources the system has left)
+#checks if resources are sufficient
+#process coins
+#check if transaction is successful
+
