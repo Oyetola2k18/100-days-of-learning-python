977359679c2f004931e9fb70f754706f9a0e2fd1
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-27 20:45:48 +0100

    Day16 starts OOP lets go

---
+++ Day16/learning/OOP.py
@@ -0,0 +1,9 @@
+#learning about object oriented programming
+
+
+#two most important things that make up an object : Its Attributes(what it has)
+#and its Functions/Methods (what it does)
+
+#we call functions ,methods in this case cuz it is a function that a particular modelled object can do
+
+#Basically with OOP , we are trying to model real life objects and those objects have attributes and methods
---
+++ Day16/learning/construcOOP.py
@@ -0,0 +1,4 @@
+#learning how to construct objects
+
+#A class is a blueprint of an object
+#an object is an an instance of a class
