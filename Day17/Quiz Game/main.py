b61f1ae5ccca4a101c51ac131134bf377593055f
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-29 22:58:04 +0100

    Day 17 almost done

---
+++ Day17/Quiz Game/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+#importing the question class
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank=[]
+for x in range(len(question_data)):
+    quest = question_data[x]["text"]
+    answer = question_data[x]["answer"]
+
+    question = Question(quest,answer)
+    question_bank.append(question)
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+print("You Have completed the Quiz")
+print(f"Your final Score Was:{quiz.score}/{len(question_bank)}")
