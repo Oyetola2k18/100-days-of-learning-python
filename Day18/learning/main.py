55ed88120a60b24fb34d06c320f859d982738b4e
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-05-30 21:07:50 +0100

    day 18 start

---
+++ Day18/learning/main.py
@@ -0,0 +1,17 @@
+#Learning more on how the the turtle library works
+
+from turtle import Turtle,Screen
+#imported the Turtle and the Screen Class
+zeph_the_turtle = Turtle()
+
+
+zeph_the_turtle.shape("turtle")
+zeph_the_turtle.pencolor(83,58,27)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
