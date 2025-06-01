27b1153f82d5e22a83b485ea7dba7b67d80e5dfb
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-01 15:02:18 +0100

    Day 18 starts

--- Day18/learning/main.py
+++ Day18/learning/main.py
@@ -1,17 +1,19 @@
 #Learning more on how the the turtle library works
 
-from turtle import Turtle,Screen
+import turtle
 #imported the Turtle and the Screen Class
-zeph_the_turtle = Turtle()
+zeph_the_turtle = turtle.Turtle()
 
 
+turtle.colormode(255)
 zeph_the_turtle.shape("turtle")
-zeph_the_turtle.pencolor(83,58,27)
+zeph_the_turtle.color("midnight blue")
+# zeph_the_turtle.pencolor(83,58,27)
+
+zeph_the_turtle.forward(100)
+zeph_the_turtle.left(270)
 
 
 
-
-
-
-screen = Screen()
+screen = turtle.Screen()
 screen.exitonclick()
