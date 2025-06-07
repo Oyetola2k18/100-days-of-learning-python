44f52898695d1fbb83767677f423f16c08f2306d
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-08 00:54:08 +0100

    setting up the main screen

---
+++ Day22/main.py
@@ -0,0 +1,9 @@
+#creating the famous pong game
+import turtle
+
+screen = turtle.Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+screen.exitonclick()
