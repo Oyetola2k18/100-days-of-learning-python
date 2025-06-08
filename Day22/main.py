548db109b57665bb74757577aea61da9c44be88d
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-08 04:02:56 +0100

    Done with the paddle class, just created the ball class

--- Day22/main.py
+++ Day22/main.py
@@ -1,36 +1,20 @@
 #creating the famous pong game
 import turtle
-
+import paddle
 screen = turtle.Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-
-#stretch_wid =20 * how tall
-#stretch_len = 20 * how wide
 screen.tracer(0)
-def up():
-    new_y = right_paddle.ycor() + 20
-    right_paddle.goto(right_paddle.xcor(),new_y)
-    # screen.update()
 
-def down():
-    new_y = right_paddle.ycor() - 20
-    right_paddle.goto(right_paddle.xcor(),new_y)
-    # screen.update()
-
-right_paddle = turtle.Turtle("square")
-# right_paddle.setheading(90)
-# right_paddle.shapesize(stretch_len=5,stretch_wid=1)
-right_paddle.shapesize(stretch_len=1,stretch_wid=5)
-right_paddle.color("white")
-right_paddle.penup()
-right_paddle.goto(x=350,y=0)
-# screen.update()
+right_paddle = paddle.Paddle((350,0))
+left_paddle = paddle.Paddle((-350,0))
 
 screen.listen()
-screen.onkey(key="Up",fun=up)
-screen.onkey(key="Down",fun=down)
+screen.onkey(key="Up",fun=right_paddle.up)
+screen.onkey(key="Down",fun=right_paddle.down)
+screen.onkey(key="w",fun=left_paddle.up)
+screen.onkey(key="s",fun=left_paddle.down)
 
 game_is_on = True
 while game_is_on:
---
+++ Day22/paddle.py
@@ -0,0 +1,24 @@
+#my paddle class
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self,location_tuple):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_len=1,stretch_wid=5)
+        self.color("white")
+        self.penup()
+        self.goto(location_tuple)
+
+    # right_paddle.setheading(90)
+    # right_paddle.shapesize(stretch_len=5,stretch_wid=1)
+   
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(),new_y)
+        # screen.update()
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(),new_y)
+        # screen.update()
