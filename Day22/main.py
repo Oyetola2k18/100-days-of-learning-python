bef991dd22a0dd687c24c35f634076c8086aff44
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-08 15:47:22 +0100

    update

--- Day22/main.py
+++ Day22/main.py
@@ -1,14 +1,20 @@
 #creating the famous pong game
 import turtle
 import paddle
+import ball
+import time
 screen = turtle.Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+
 screen.tracer(0)
 
 right_paddle = paddle.Paddle((350,0))
 left_paddle = paddle.Paddle((-350,0))
+game_ball = ball.Ball()
+
+
 
 screen.listen()
 screen.onkey(key="Up",fun=right_paddle.up)
@@ -18,5 +24,7 @@ screen.onkey(key="s",fun=left_paddle.down)
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    game_ball.move()
 screen.exitonclick()
--- Day22/paddle.py
+++ Day22/paddle.py
@@ -8,7 +8,7 @@ class Paddle(Turtle):
         self.shapesize(stretch_len=1,stretch_wid=5)
         self.color("white")
         self.penup()
-        self.goto(location_tuple)
+        self.teleport(x=location_tuple[0], y= location_tuple[1])
 
     # right_paddle.setheading(90)
     # right_paddle.shapesize(stretch_len=5,stretch_wid=1)
