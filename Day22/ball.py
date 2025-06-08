d722bfbf4d691b0f1655a7dd937747154afeacac
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-08 15:45:27 +0100

    Added the Ball, next on the list is to add the Moving ball loginc

--- Day22/ball.py
+++ Day22/ball.py
@@ -1,1 +1,14 @@
 #my ball class
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.shape("circle")
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(x=new_x,y=new_y)
