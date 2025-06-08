e934086472c9b816cf842d23888ccbdf61937390
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-08 01:41:11 +0100

    Added 1 paddle to the game

--- Day22/main.py
+++ Day22/main.py
@@ -6,4 +6,33 @@ screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 
+#stretch_wid =20 * how tall
+#stretch_len = 20 * how wide
+screen.tracer(0)
+def up():
+    new_y = right_paddle.ycor() + 20
+    right_paddle.goto(right_paddle.xcor(),new_y)
+    # screen.update()
+
+def down():
+    new_y = right_paddle.ycor() - 20
+    right_paddle.goto(right_paddle.xcor(),new_y)
+    # screen.update()
+
+right_paddle = turtle.Turtle("square")
+# right_paddle.setheading(90)
+# right_paddle.shapesize(stretch_len=5,stretch_wid=1)
+right_paddle.shapesize(stretch_len=1,stretch_wid=5)
+right_paddle.color("white")
+right_paddle.penup()
+right_paddle.goto(x=350,y=0)
+# screen.update()
+
+screen.listen()
+screen.onkey(key="Up",fun=up)
+screen.onkey(key="Down",fun=down)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 screen.exitonclick()
