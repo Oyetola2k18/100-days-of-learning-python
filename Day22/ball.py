80c97ffbff4175fa774db2aa97efa633ed6671dc
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-09 16:20:28 +0100

    Day 22 done

--- Day22/ball.py
+++ Day22/ball.py
@@ -7,8 +7,27 @@ class Ball(Turtle):
         self.penup()
         self.color("white")
         self.shape("circle")
+        self.x_move = 10 #rate of movement 
+        self.y_move = 10 #rate of movement
+        self.move_speed = 0.1
+
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() +self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(x=new_x,y=new_y)
+    
+    def bounce_y(self):
+        self.y_move = self.y_move * -1
+
+    def bounce_x(self):
+        self.x_move = self.x_move * -1
+        if self.move_speed > 0:
+            self.move_speed *= 0.9
+        else:
+            pass
+    
+    def reset_position(self):
+        self.goto(0,0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- Day22/main.py
+++ Day22/main.py
@@ -3,6 +3,7 @@ import turtle
 import paddle
 import ball
 import time
+import scoreboard
 screen = turtle.Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
@@ -13,9 +14,7 @@ screen.tracer(0)
 right_paddle = paddle.Paddle((350,0))
 left_paddle = paddle.Paddle((-350,0))
 game_ball = ball.Ball()
-
-
-
+score = scoreboard.Scoreboard()
 screen.listen()
 screen.onkey(key="Up",fun=right_paddle.up)
 screen.onkey(key="Down",fun=right_paddle.down)
@@ -24,7 +23,32 @@ screen.onkey(key="s",fun=left_paddle.down)
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    
+    time.sleep(game_ball.move_speed)
     screen.update()
     game_ball.move()
+    #detect collision with the walls 
+    if game_ball.ycor() > 280 or game_ball.ycor() < -280:
+        #then bounce
+        game_ball.bounce_y()
+
+    
+    #detect collision for the  paddles
+    if (game_ball.distance(right_paddle) < 50 and game_ball.xcor() > 320) or (game_ball.distance(left_paddle) < 50 and game_ball.xcor() < -320):
+        game_ball.bounce_x()
+    # else:
+        # game_ball.teleport(0,0)
+        # game_ball.x_move *= -1
+        # game_ball.y_move *= -1
+    
+    #right paddle misses
+    if game_ball.xcor() > 390:
+        game_ball.reset_position()
+        score.l_point()
+    
+
+    #left paddle misses
+    if game_ball.xcor() < -390:
+        game_ball.reset_position()
+        score.r_point()
 screen.exitonclick()
