92eebbb64f548ba61e4a13b5679995e38b6e7a6d
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-13 22:26:43 +0100

    Day 25 final project almost done

--- Day25/US-STATES-GAME/main.py
+++ Day25/US-STATES-GAME/main.py
@@ -9,14 +9,14 @@ screen.title("US STATES GUESSING GAME")
 #turtle has a function that allows us to load in a shape
 image = "Day25/US-STATES-GAME/blank_states_img.gif"
 screen.addshape(image)
-
+screen.setup(width=720,height=520)
 #code used to get different points on the map mapped to their location on the map
 # def get_mouse_click_coor(x,y):
 #     print(x,y)
 # turtle.onscreenclick(get_mouse_click_coor)
 
 turtle.shape(image)
-
+ 
 location  = "Day25/US-STATES-GAME/50-USA-STATES.csv"
 data = pd.read_csv(location)
 
@@ -26,10 +26,13 @@ writer.penup()
 
 data_to_list = data["state"].to_list()
 num_of_questions_done = 0
-answer_state = (screen.textinput(title="Guess the state", prompt="What's another state's name?")).capitalize()
+answer_state = (screen.textinput(title="Guess the state", prompt="What's another state's name?")).title()
 game_is_on = True
 guessed_states = []
 while game_is_on:
+    if len(guessed_states) == 50:
+        game_is_on = False
+
     if answer_state not in guessed_states:
         if answer_state in data_to_list:
             state_row= data[data.state == answer_state]
@@ -41,7 +44,7 @@ while game_is_on:
             num_of_questions_done +=1
             guessed_states.append(answer_state)
 
-    answer_state = (screen.textinput(title=f"Guessed {num_of_questions_done}/50", prompt="What's another state's name?")).capitalize()
+    answer_state = (screen.textinput(title=f"Guessed {num_of_questions_done}/{len(data_to_list)}", prompt="What's another state's name?")).title()
     
 
 screen.mainloop()
