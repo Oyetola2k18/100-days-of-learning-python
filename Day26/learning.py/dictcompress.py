4e56c804a2dd3941c8983fd54e0b18683c40e595
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-16 19:04:54 +0100

    still not done with day26

--- Day26/learning.py/dictcompress.py
+++ Day26/learning.py/dictcompress.py
@@ -1,2 +1,11 @@
 #list comprehension but in dictionaries
-#should we say dictionary compression lol
+#should we say dictionary comprehension lol
+#syntax:new_value for item in list}
+#or
+# new_dict = {new_key
+# new_dict = {new_key:new_value for (key,value) in dict.items()}
+#to add conditions
+# new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+
+
+name = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
