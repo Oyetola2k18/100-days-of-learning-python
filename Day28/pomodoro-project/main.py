0de44bab1c25ec14c336aa2d6d050264fd252bc9
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-20 15:03:37 +0100

    day 26 still going

--- Day28/pomodoro-project/main.py
+++ Day28/pomodoro-project/main.py
@@ -1,3 +1,4 @@
+import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,3 +16,30 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas =tkinter.Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
+#add image to canvas
+#the create image function takes in 3 values, the x and y coord to know where the image would be placed
+#and the image we want to use, we cant just pass in the image  path in there , we need to make use of a class
+#called photoimage that we will use to read that image from its path(if that makes sense)
+tomato_image = tkinter.PhotoImage(file="Day28/pomodoro-project/tomato.png")
+canvas.create_image(100,112,image=tomato_image)
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,28,"bold"))
+canvas.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
--- Day28/readme.txt
+++ Day28/readme.txt
@@ -1,1 +1,6 @@
 i will be working on building a pomodoro timer(maybe i can use it to improve my productivity)
+
+
+
+#convas widget
+in the tkinter library allows us to layer things one ontop the other(like drawing)
