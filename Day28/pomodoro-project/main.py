f5defd38d84d629c8967f53c7d43c40d610b879e
Oyetola2k18 <ogunyemioyetola28@gmail.com>
2025-06-20 20:27:21 +0100

    done with the UI of the pomodoro timer

--- Day28/pomodoro-project/main.py
+++ Day28/pomodoro-project/main.py
@@ -9,6 +9,7 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -28,18 +29,29 @@ canvas =tkinter.Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
 tomato_image = tkinter.PhotoImage(file="Day28/pomodoro-project/tomato.png")
 canvas.create_image(100,112,image=tomato_image)
 canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,28,"bold"))
-canvas.pack()
+canvas.grid(row=1,column=1)
+
+#we will not use bg to change the color of a label text, we use fg which stands for
+
+#Heading TImer label
+timer_heading = tkinter.Label(text="Timer")
+timer_heading.config(font=(FONT_NAME,45,"bold"), fg=GREEN, bg=YELLOW)
+timer_heading.grid(row=0,column=1)
 
 
+#start button
+start = tkinter.Button(text="Start",highlightthickness=0)
+start.grid(row=2,column=0)
+# start.config(padx=0,pady=0)
 
 
+#Reset button
+reset = tkinter.Button(text="Reset", highlightthickness=0)
+reset.grid(row=2,column=2)
+# reset.config(padx=0,pady=0)
 
-
-
-
-
-
-
-
-
+#checkbox to count how many pomodoro's you've done
+check = tkinter.Label(text="✔️")
+check.grid(row=3,column=1)
+check.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME,18,"bold"))
 window.mainloop()
